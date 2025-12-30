99139e072cc7be0200fd9fb9d704dd2a86b3fe16
yeabsirasahle <yeabsirasahle22@gmail.com>
2025-12-30 22:39:01 +0300

    Add files via upload

---
+++ src/Instructor.java
@@ -0,0 +1,11 @@
+public class Instructor {
+    int id;
+    String name;
+    String password;
+
+    public Instructor(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+}
