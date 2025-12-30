07a286366d884825144da4b61a581e62b07ee892
booruch <burukbirhanu28@gmail.com>
2025-12-30 15:23:54 +0300

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int id;
+    String name;
+    String password;
+
+    public Student(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+}
