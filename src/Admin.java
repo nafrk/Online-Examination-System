14085723b0ae4ac108e2e7fdd5a5c823c16fadbc
gutabeti <bettyguta778@gmail.com>
2025-12-30 14:50:35 +0300

    AdminModule: Created Admin.java model class
    Added Admin entity with fields and getters/setters
    to support admin authentication and management.

---
+++ src/Admin.java
@@ -0,0 +1,14 @@
+public class Admin {
+    int id;
+    String name;
+    String password;
+    String phone;
+
+    public Admin(int id, String name, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+    }
+}
+
