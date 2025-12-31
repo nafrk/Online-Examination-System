e6b1ac930c893bd20eaa124d87d43ae87fe93530
tomstud46 <chentomas46@gmail.com>
2025-12-31 21:18:21 +0300

    Add files via upload
    "ExamModule: Created Exam.java and Question.java model classes"

---
+++ src/Exam.java
@@ -0,0 +1,11 @@
+public class Exam {
+    int examId;
+    String title;
+    int duration;
+
+    public Exam(int examId, String title, int duration) {
+        this.examId = examId;
+        this.title = title;
+        this.duration = duration;
+    }
+}
---
+++ src/Question.java
@@ -0,0 +1,7 @@
+public class Question {
+    int id;
+    int examId;
+    String question;
+    String correctAnswer;
+    String explanation;
+}
